97cc03a466a721e2dac8c634188f5203be434c37
yoginpahuja <yoginpahuja69@gmail.com>
2023-11-23 04:18:49 +0530

    Added: Included Line Equality checking method

--- LineConsProb.java
+++ LineConsProb.java
@@ -5,21 +5,48 @@ public class LineConsProb {
     {
         return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
     }
+    public static boolean equalityCheck(double len1,double len2)
+    {
+        return len1==len2;
+    }
+    public static void printLengths(double len,int lineNum)
+    {
+        DecimalFormat df = new DecimalFormat("###.###");
+        System.out.println("Length of the line " + lineNum + " is " + df.format(len));
+    }
     public static void main (String[] args)
     {
         Scanner scanObj=new Scanner(System.in);
         scanObj.useDelimiter("[\\s,]+");
         System.out.println("Welcome to Line Comparison Computation Program");
-        System.out.println("Please enter the coordinates of the first point (x1,y1)");
+
+        System.out.println("Please enter the coordinates of the first point of 1st Line (x1,y1)");
         int x1,y1;
         x1=scanObj.nextInt();
         y1=scanObj.nextInt();
-        System.out.println("Please enter the coordinates of the second point (x2,y2)");
+        System.out.println("Please enter the coordinates of the second point of 1st Line (x2,y2)");
         int x2,y2;
         x2=scanObj.nextInt();
         y2=scanObj.nextInt();
-        double len=length(x1,x2,y1,y2);
-        DecimalFormat df = new DecimalFormat("###.###");
-        System.out.println("Length of the line is " + df.format(len));
+        
+        System.out.println("Please enter the coordinates of the first point of 2nd Line (x3,y3)");
+        int x3,y3;
+        x3=scanObj.nextInt();
+        y3=scanObj.nextInt();
+        System.out.println("Please enter the coordinates of the second point of 2nd Line (x4,y4)");
+        int x4,y4;
+        x4=scanObj.nextInt();
+        y4=scanObj.nextInt();
+
+        double len1=length(x1,x2,y1,y2);
+        double len2=length(x3,x4,y3,y4);
+   
+        printLengths(len1,1);
+        printLengths(len2,2);
+
+        if(equalityCheck(len1, len2))
+            System.out.println("Lines are equal");
+        else
+            System.out.println("Lines are not equal");
     }
 }
